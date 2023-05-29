4dcd2b6f24d55a8d5554d0d4885ac59b807180ef
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-05-29 20:30:00 -0300

    First commit

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,14 @@
+export const PostCard = (props) => {
+    // ({post})
+    // ({ title, cover, body, id })
+    // const { post } = propst;
+    return (
+        <div className='post'>
+            <img key={props.cover.id} src={props.cover} alt={props.title} />
+                <div key={props.id} className='post-content'>
+                    <h1>{props.title}</h1>
+                    <p>{props.body}</p>
+                </div>
+        </div>
+    )
+}
